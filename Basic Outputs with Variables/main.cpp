68bcae82ec0ace0b8b3f4f11101c69679f88bf30
Bailey Borchers <baileyborchers@gmail.com>
2021-06-26 16:36:13 -0700

    Added ascii art

--- Basic Outputs with Variables/main.cpp
+++ Basic Outputs with Variables/main.cpp
@@ -24,6 +24,15 @@ int main() {
     cout << userNum << " + " << userNum2 << " = " << userNum + userNum2 << endl;
     cout << userNum << " * " << userNum2 << " = " << userNum * userNum2 << endl;
     
+    cout << "   *   " << endl;
+    cout << "  ***  " << endl;
+    cout << " ***** " << endl;
+    cout << "*******" << endl;
+    cout << "  ***  " << endl << endl << endl;
+    cout << " /\\   /\\" << endl;
+    cout << "   o o   " << endl;
+    cout << "  =   =  " << endl;
+    cout << "   ---   " << endl;
     
     
    return 0;

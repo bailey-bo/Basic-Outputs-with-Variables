ddb094b283756faf504bea1a3417e0245d16bcae
Bailey Borchers <baileyborchers@gmail.com>
2021-06-26 16:18:40 -0700

    Initial Commit

---
+++ Basic Outputs with Variables/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Basic Outputs with Variables
+//
+//  Created by Bailey Borchers on 6/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
